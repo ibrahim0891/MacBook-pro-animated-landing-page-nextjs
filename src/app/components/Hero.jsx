8ed0f499919097d60230434f9bf85f79e6a4a516
ibrahim0891 <nijhum0891@gmail.com>
2025-10-28 19:50:01 +0600

    mp

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+    let videoRef = useRef()
+
+    useEffect(() => {
+      if(videoRef.current){
+        videoRef.current.playbackRate = 2; 
+      }
+    },[])
+    return (
+        <section 
+            id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img draggable="false" src="/title.png" alt="Macbook Title" />
+                <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline></video>
+                <button> Buy</button>
+                <p>From $1599 or $133/mo for 12 months</p>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
  
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
     return (
       <main>
         <NavBar></NavBar>
+        <Hero/>
       </main>
     );
 }
