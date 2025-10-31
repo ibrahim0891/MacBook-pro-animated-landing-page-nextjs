c51c83da41727aed7a0d9673628b49c0629f4040
ibrahim0891 <nijhum0891@gmail.com>
2025-10-31 21:14:35 +0600

    fixed perspactive issues in product viewwe section

--- src/app/components/ProductViewer.jsx
+++ src/app/components/ProductViewer.jsx
@@ -1,26 +1,26 @@
 "use client";
-
+import useMacbookStore from "../store";
 import clsx from "clsx";
- 
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
-import MacbookModel14 from "./models/Macbook-14";
-import StudioLights from "./three/StudioLights";
-import ModelSwitcher from "./three/ModelSwitcher";
+import MacbookModel14 from "./models/Macbook-14.jsx";
+
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
 import { useMediaQuery } from "react-responsive";
-import useMacbookStore from "../store";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
-    const isMobile = useMediaQuery({ query : '(max-width : 1024px)' });
+
+    const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+
     return (
         <section id='product-viewer'>
-            <h2 className="text-center">Take a closer look</h2>
+            <h2>Take a closer look.</h2>
+
             <div className='controls'>
-                <p className='info'>
-                    {" "}
-                    Macbook Pro {scale == 0.06 ? '14"' : '16"'} in Space black
-                </p>
+                {/*<p className="info">Macbook Pro | Available in 14" & 16" in Space Gray & Dark colors</p>*/}
+
                 <div className='flex-center gap-5 mt-5'>
                     <div className='color-control'>
                         <div
@@ -29,15 +29,16 @@ const ProductViewer = () => {
                                 "bg-neutral-300",
                                 color === "#adb5bd" && "active"
                             )}
-                        ></div>
+                        />
                         <div
                             onClick={() => setColor("#2e2c2e")}
                             className={clsx(
                                 "bg-neutral-900",
                                 color === "#2e2c2e" && "active"
                             )}
-                        ></div>
+                        />
                     </div>
+
                     <div className='size-control'>
                         <div
                             onClick={() => setScale(0.06)}
@@ -47,7 +48,7 @@ const ProductViewer = () => {
                                     : "bg-transparent text-white"
                             )}
                         >
-                            <p> 14"</p>
+                            <p>14"</p>
                         </div>
                         <div
                             onClick={() => setScale(0.08)}
@@ -57,18 +58,24 @@ const ProductViewer = () => {
                                     : "bg-transparent text-white"
                             )}
                         >
-                            <p> 16"</p>
+                            <p>16"</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <Canvas id='canvas'>
-              <StudioLights/>
-                {/* <MacbookModel14 color={color} scale={scale} /> */}
-                <ModelSwitcher isMobile={isMobile} scale={isMobile ? scale - 0.03 : scale} />
+
+            <Canvas
+                id='canvas'
+                camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+            >
+                <StudioLights />
+
+                <ModelSwitcher
+                    scale={isMobile ? scale - 0.03 : scale}
+                    isMobile={isMobile}
+                />
             </Canvas>
         </section>
     );
 };
-
 export default ProductViewer;
